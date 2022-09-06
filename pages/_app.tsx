e5f5b520331c70e51f8023328392f49c1009cb2c
ppasta90 <ppastacaldi@outlook.it>
2022-09-07 00:14:14 +0200

    chore: configure tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extends: {},
+  },
+  plugins: [],
+};
